d02d8a4ac6755a7bf7f2348b7f1c9d73868902e3
Sarvin Nami <srvn0nm@gmail.com>
2023-01-07 19:50:21 +0330

    starting the logic of simple factoriel counter

--- factoriel.s
+++ factoriel.s
@@ -1,5 +1,15 @@
 	AREA myData, DATA
 		CONST EQU 4
-			
-	AREA myCode, CODE, READONLY
 		
+	EXPORT __main
+	AREA myCode, CODE
+		
+		
+__main
+	LDR r0, =CONST
+	LDR r1,=1
+	LDR r2,=1
+	
+loop
+	MUL r1,r2,r1
+	ADD
