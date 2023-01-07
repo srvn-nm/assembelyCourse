bad39e95cde9d2e5514a048b7d330fcd3a2f85c4
Sarvin Nami <srvn0nm@gmail.com>
2023-01-07 19:55:06 +0330

    building the project successfully

--- factoriel.s
+++ factoriel.s
@@ -7,9 +7,17 @@
 		
 __main
 	LDR r0, =CONST
-	LDR r1,=1
-	LDR r2,=1
+	LDR r1, =1
+	LDR r2, =1
 	
 loop
-	MUL r1,r2,r1
-	ADD
+	MUL r1, r2, r1
+	ADD r2, r2, #1
+	
+	SUBS r4, r0, r2
+	
+	BNE loop
+	MUL r1, r2, r1
+	
+	HERE B HERE
+	END
